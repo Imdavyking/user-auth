02ee0fe6370ded30901602cbb19b25d5cb74e973
DavyKing <yungdynamic53@gmail.com>
2022-08-19 22:51:22 +0100

    thank God

--- create-link-shorter-table.php
+++ create-link-shorter-table.php
@@ -3,8 +3,8 @@ require_once "./db.php";
 
 $stmt = $GLOBALS['conn']->prepare("CREATE TABLE IF NOT EXISTS linkshortener (
       id INT NOT NULL AUTO_INCREMENT,
-      shortLink VARCHAR(255) NOT NULL,
-      actualLink VARCHAR(255) NOT NULL,
+      shortLink TEXT NOT NULL,
+      actualLink TEXT NOT NULL,
       PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4");
 $stmt->execute();
